54242f2e5e42a9fe9e1a77791c9e9a649fb13a9b
aryanoberoi-fanon <1MS20IS025@msrit.edu>
2025-07-19 14:30:43 +0530

    first again

---
+++ greenfinite/src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CustomCursor from './components/ui/Cursor'
+import Analyze from './pages/Analyze'
+function App() {
+  return (
+
+    <div className="w-screen h-screen flex flex-col">
+    <CustomCursor />
+
+      <div className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/analyze" element={<Analyze />} />
+        </Routes>
+      </div>
+      
+    </div>
+  )
+}
+
+
+export default App
