416a9a21d25d9bf217f30ba0c0a81f74c7a6d5bd
Ayush singh <143987677+ayushsingh7982@users.noreply.github.com>
2025-08-06 10:12:52 +0530

    Newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import CustomCursor from './components/ui/Cursor'
-import Analyze from './pages/Analyze'
-function App() {
-  return (
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import CustomCursor from './components/ui/Cursor';
+import Analyze from './pages/Analyze';
+import NewsletterPopup from './components/NewsletterPopup';
 
+function App() {
+  // const location = useLocation(); // ✅ Get current route
+
+  return (
     <div className="w-screen h-screen flex flex-col">
-    <CustomCursor />
+      <CustomCursor />
+
+      {/* ✅ Show popup only on Home page AND only if user hasn't seen it */}
+      {/* {location.pathname === '/' && <NewsletterPopup />} */}
 
       <div className="flex-1">
         <Routes>
@@ -14,10 +20,8 @@ function App() {
           <Route path="/analyze" element={<Analyze />} />
         </Routes>
       </div>
-      
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
